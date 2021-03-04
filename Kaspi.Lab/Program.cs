b4e343b96452dbc43d133504e46db3480fcc37bc
Issabek <180107287@stu.sdu.edu.kz>
2021-03-04 16:24:55 +0600

    Hello World

---
+++ Kaspi.Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kaspi.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
